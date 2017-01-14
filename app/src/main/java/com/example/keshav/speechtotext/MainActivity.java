0684c93ccb54f25e3c4cacfabe920b22e96c75e3
keshav <keshavchen@gmail.com>
2017-01-14 14:36:54 +0530

    .

--- app/src/main/java/com/example/keshav/speechtotext/MainActivity.java
+++ app/src/main/java/com/example/keshav/speechtotext/MainActivity.java
@@ -46,10 +46,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void speechtotext(){
+
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Speak now..");
+
         try{
             startActivityForResult(intent,SPEECH_RECOGNITION_CODE);
         }
@@ -62,15 +64,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode){
-            case SPEECH_RECOGNITION_CODE: {
-                if(resultCode == RESULT_OK && null!= data){
+        if(requestCode == SPEECH_RECOGNITION_CODE) {
+                if(resultCode == RESULT_OK && null!= data) {
                     ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                     String text = result.get(0);
                     txtOutput.setText(text);
                 }
-                break;
-            }
+
         }
     }
 }
+
